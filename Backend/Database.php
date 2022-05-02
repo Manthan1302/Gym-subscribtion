be3479513edc12ecbf94ab293bd9aa0fe6c4673a
rohit <rohitnitaibera@gmail.com>
2022-05-02 16:18:35 +0530

    backend added

--- Backend/Database.php
+++ Backend/Database.php
@@ -14,13 +14,24 @@
     if($mydb){
         echo " new database created ";
     }else{
-        echo " database already exist";
+        echo " gym database already exist!";
     }
 
-    // // creating tables
-    // $dbname = "gyms";
+    // creating tables
+    $dbname = "gyms";
+    $connection = mysqli_connect($host,$user,$pass,$dbname);
 
-    // $connect = mysqli_connect($host,)
+    // User table
+    
+    $userTableQuery = "create table user( id int primary key , firstname varchar(50) , lastname varchar(50) , email varchar(50) , postcode varchar(10) ,password varchar(20) )";
+
+    $userTableCreated = mysqli_query($connection , $userTableQuery);
+    
+    if($userTableCreated){
+        echo "<br /> user table created";
+    }else{
+        echo "<br /> user table exist!";
+    }
 
 
 
