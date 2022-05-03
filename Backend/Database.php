fcbc686d848c270d7d84f2a574ec7f2aa5334508
rohit <rohitnitaibera@gmail.com>
2022-05-03 20:43:11 +0530

    backend added

--- Backend/Database.php
+++ Backend/Database.php
@@ -8,7 +8,7 @@
     $server = mysqli_connect($host , $user , $pass);
     
 
-    $dbquery ="create database gyms";
+    $dbquery ="create database gym_database";
     $mydb = mysqli_query($server , $dbquery );
 
     if($mydb){
@@ -18,21 +18,29 @@
     }
 
     // creating tables
-    $dbname = "gyms";
+    $dbname = "gym_database";
     $connection = mysqli_connect($host,$user,$pass,$dbname);
 
     // User table
-    
     $userTableQuery = "create table user( id int primary key , firstname varchar(50) , lastname varchar(50) , email varchar(50) , postcode varchar(10) ,password varchar(20) )";
 
-    $userTableCreated = mysqli_query($connection , $userTableQuery);
+    $userTable = mysqli_query($connection , $userTableQuery);
     
-    if($userTableCreated){
+    if($userTable){
         echo "<br /> user table created";
     }else{
         echo "<br /> user table exist!";
     }
 
+    //  gym table
+    $gymTableQuery = "create table gym( gymId int primary key , gymName varchar(50) , gymAddress varchar(50) , gymLocation varchar(50) , gymImages JSON , gymPass JSON , gymEquipment JSON , gymAmenities JSON , aboutGym varchar(400))";
 
+    $gymTable = mysqli_query($connection , $gymTableQuery);
+
+    if($gymTable){
+        echo "<br> gym table created";
+    }else{
+        echo "<br> gym table exist";
+    }
 
 ?>
