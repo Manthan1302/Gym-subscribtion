d10c9ac549c1cb3450384518bcb37f4ba02caf9c
rohit <rohitnitaibera@gmail.com>
2022-05-12 15:22:35 +0530

    changes commited

--- Backend/Database.php
+++ Backend/Database.php
@@ -3,18 +3,19 @@
     // creating database
     $host = "localhost";
     $user = "root";
-    $pass = "";
+    $pass = "rohit1979";
 
     $server = mysqli_connect($host , $user , $pass);
     
+
     $dbquery ="create database gym_database";
     $mydb = mysqli_query($server , $dbquery );
 
-    // if($mydb){
-    //     echo " new database created ";
-    // }else{
-    //     echo " gym database already exist!";
-    // }
+    if($mydb){
+        echo " new database created ";
+    }else{
+        echo " gym database already exist!";
+    }
 
 
     // creating tables
@@ -28,11 +29,11 @@
 
     $userTable = mysqli_query($connection , $userTableQuery);
     
-    // if($userTable){
-    //     echo "<br /> user table created";
-    // }else{
-    //     echo "<br /> user table exist!";
-    // }
+    if($userTable){
+        echo "<br /> user table created";
+    }else{
+        echo "<br /> user table exist!";
+    }
 
 
     //feedback table
@@ -41,11 +42,11 @@
 
     $feedbackTableCreated = mysqli_query($connection , $feedbackTableQuery);
    
-    // if($feedbackTableCreated){
-    //     echo "<br /> feedback table created";
-    // }else{
-    //     echo "<br /> feedback table exist!";
-    // }
+    if($feedbackTableCreated){
+        echo "<br /> feedback table created";
+    }else{
+        echo "<br /> feedback table exist!";
+    }
 
 
     //feedback fk updated
@@ -53,58 +54,78 @@
 
    $feedbackTableCreated2 = mysqli_query($connection , $feedFk);
 
+   if($feedbackTableCreated2){
+    echo "<br /> feedback table fk updated";
+    }else{
+    echo "<br /> feedback table fk not updated!";
+    }
 
-//    if($feedbackTableCreated2){
-//     echo "<br /> feedback table fk updated";
-//     }else{
-//     echo "<br /> feedback table fk not updated!";
-//     }
 
+    // review table
+    $reviewTableQuery = "create table review(review_id int primary key,review_points float,user_id int )";
 
+    $reviewTableCreated = mysqli_query($connection,$reviewTableQuery);
+
+    if($reviewTableCreated){
+        echo "<br /> review table created";
+    }else{
+        echo "<br /> review table exist!";
+    }
+
+    // review table fk updated
+    $reviewFk= "ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`userId`) ON DELETE RESTRICT ON UPDATE RESTRICT";
+
+   $reviewTableCreated2 = mysqli_query($connection , $reviewFk);
+
+   if($reviewTableCreated2){
+    echo "<br /> review table fk updated";
+    }else{
+    echo "<br /> review table fk not updated!";
+    }
 
     //  gym table
     $gymTableQuery = "create table gym( gymId int primary key , gymName varchar(50) , gymAddress varchar(50) , gymLocation varchar(50) , gymImages JSON , gymPass JSON , gymEquipment JSON , gymAmenities JSON , aboutGym varchar(400))";
 
     $gymTable = mysqli_query($connection , $gymTableQuery);
 
-    // if($gymTable){
-    //     echo "<br> gym table created";
-    // }else{
-    //     echo "<br> gym table exist";
-    // }
+    if($gymTable){
+        echo "<br> gym table created";
+    }else{
+        echo "<br> gym table exist";
+    }
 
     // subscription pass table
     $subscription = "create table pass( passId int primary key , passName varchar(20) , passDiscription varchar(50), passPrice int, passType varchar(100) , user_id int , gym_id int )";
 
     $subTable = mysqli_query($connection , $subscription);
 
-    // if($subTable){
-    //     echo "<br> subscription table created";
-    // }else{
-    //     echo "<br> subscription table exist";
-    // }
+    if($subTable){
+        echo "<br> subscription table created";
+    }else{
+        echo "<br> subscription table exist";
+    }
 
     // user fk updated
     $subFk= "ALTER TABLE `pass` ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`userId`) ON DELETE RESTRICT ON UPDATE RESTRICT";
 
     $subscriptionTableCreated2 = mysqli_query($connection , $subFk);
 
-//    if($subscriptionTableCreated2){
-    // echo "<br /> subscription table fk updated";
-    // }else{
-    // echo "<br /> subscription table fk not updated!";
-    // }
+   if($subscriptionTableCreated2){
+    echo "<br /> subscription table fk updated";
+    }else{
+    echo "<br /> subscription table fk not updated!";
+    }
 
     // gym fk update
     $subFk2= "ALTER TABLE `pass` ADD FOREIGN KEY (`gym_id`) REFERENCES `gym`(`gymId`) ON DELETE RESTRICT ON UPDATE RESTRICT";
 
     $subscriptionTableCreated3 = mysqli_query($connection , $subFk2);
 
-//    if($subscriptionTableCreated3){
-//     echo "<br /> subscription table fk updated";
-//     }else{
-//     echo "<br /> subscription table fk not updated!";
-//     }
+   if($subscriptionTableCreated3){
+    echo "<br /> subscription table fk updated";
+    }else{
+    echo "<br /> subscription table fk not updated!";
+    }
 
 
     //  database variables
--- Frontend/components/register.php
+++ Frontend/components/register.php
@@ -1,3 +1,26 @@
+<?php 
+
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "rohit1979";
+$dbname = "gym_database";
+
+$connection = mysqli_connect($host,$user,$pass,$dbname);
+
+if($connection){
+    echo '<script>';
+    echo 'console.log(" database connection established")';
+    echo '</script>';
+}else{
+    echo '<script>';
+    echo 'console.log(" something went wrong in db connection")';
+    echo '</script>';
+}
+
+?>
+
 <html>
     <head>
         <link rel="stylesheet" href="../style/register.css" type="text/css">
@@ -5,25 +28,24 @@
    
 
 <body>
-<div class="nav">
-    <div class="nav-left">Gyms</div>
-    <div class="nav-right"><a href="login.php"><button class="nav-button">Login</button></a>
+    <div class="nav">
+        <div class="nav-left">Gyms</div>
+        <div class="nav-right"><button class="nav-button"><a href="login.php"> Login </a></button></div>
+    </div> 
+    <br>
+    <div class="login">
+        <h2>Register</h2><br>
     </div>
-</div> 
-<br>
-<div class="login">
-    <h2>Register</h2><br>
-</div>
     <div class="form"><h2>Register with Email</h2>
-    <form>
-        <input type="text" name="email" placeholder="First Name" class="textbox-f">
-        <input type="text" name="email" placeholder="Last Name" class="textbox-f"><br><br>
-        <input type="text" name="email" placeholder="Email" class="textbox"><br><br>
-        <input type="text" name="password" placeholder="Password" class="textbox"><br><br>
-        <input type="text" name="email" placeholder="Postcode" class="textbox-f">
+    <form method="post">
+        <input type="text" name="firstname" placeholder="First Name" class="textbox-f" />
+        <input type="text" name="lastname" placeholder="Last Name" class="textbox-f" /><br><br>
+        <input type="text" name="email" placeholder="Email" class="textbox" /><br><br>
+        <input type="password" name="password" placeholder="Password" class="textbox" /><br><br>
+        <input type="text" name="postcode" placeholder="Postcode" class="textbox-f" />
         <p class="forPass">
             Already have a Hussle account? Log in.</p><br><br>
-        <button class="login-btn">Register</button>
+        <button class="login-btn" name="register">Register</button>
     </form>
     </div>
 
@@ -49,4 +71,74 @@
         </div>
 
     </div>
+
+</body>
 </html>
+
+
+
+<?php
+
+if(isset($_POST['register'])){
+
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $postcode = $_POST['postcode'];
+    $uid = rand(1000 , 9999);
+
+    // echo "firstname : $fname <br>";
+    // echo "lastename : $lname <br>";
+    // echo "email : $email <br>";
+    // echo "password : $password <br>";
+    // echo "postcode : $postcode <br>";
+
+    if(empty($fname) || empty($lname) || empty($email) || empty($password) || empty($postcode)){
+        echo '<script>';
+        echo "alert('all details are important')";
+        echo '</script>';   
+    }else{
+        if(!preg_match("/^[a-zA-Z]+$/",$fname)){
+            echo '<script>';
+            echo "alert('all details are important')";
+            echo '</script>'; 
+        }elseif (!preg_match("/^[a-zA-Z]+$/",$lname)){
+            echo '<script>';
+            echo "alert('all details are important')";
+            echo '</script>'; 
+        }elseif (!preg_match("/^[0-9]+$/",$password)){
+            echo '<script>';
+            echo "alert('all details are important')";
+            echo '</script>'; 
+        }elseif (!preg_match("/^[0-9]+$/",$postcode)){
+            echo '<script>';
+            echo "alert('all details are important')";
+            echo '</script>'; 
+        }else{
+            $_SESSION['userid'] = $uid;
+            $_SESSION['firstname'] =  $fname ;
+            $_SESSION['lastname'] = $lname ;
+            $_SESSION['email'] = $email ;
+            $_SESSION['password'] = $password ;
+            $_SESSION['postcode'] = $postcode ;
+
+
+            $insertquery = "insert into user values('$uid','$fname','$lname','$email','$postcode','$password') ";
+
+            $registration = mysqli_query($connection , $insertquery);
+
+            if($registration){
+                header("Location:profile.php");
+            }else{
+                echo '<script>';
+                echo "alert('server error:404 , something went wrong')";
+                echo '</script>';
+            }
+
+        }
+    }
+
+}
+
+?>
